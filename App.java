b46377bd300eff7bea12b618587a3c96e28ffa9c
Silvério Benfica <vedderade@hotmail.com>
2025-09-16 19:54:21 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,22 @@
+
+public class App {
+public static void main(String[] args) {
+    String sala = "A219";
+    String salainformada = "";
+    Scanner scanner = new scanner(System.in);
+    
+    System.out.println(x: "Digite o numero da sala:");
+    salainformada == scanner.next();
+    
+    if (salainformada.equals(salainformada));{
+    System.out.println(x: "você encontrou a sua sala");
+    
+
+else
+{
+    System.out.println(x:"você esta perdido");
+    
+}
+    scanner.close();
+    
+}
